7f0d5c4e8f9e27e769c4fd8e51b95d58cb83bf9a
Ivan Nosov <ivan7en@gmail.com>
2022-08-04 11:04:42 +0300

    Add transaction and user associations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,11 +1,5 @@
 class Transaction < ApplicationRecord
+  belongs_to :sender,   class_name: 'User'
+  belongs_to :recipient, class_name: 'User'
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
-
-  def sender
-    User.find_by(id: sender_id)
-  end
-
-  def recipient
-    User.find_by(id: recipient_id)
-  end
 end
